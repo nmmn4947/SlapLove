7de857616f50ac895dec86bae04bf4226ead822e
nmmn4947 <nmmn4947@gmail.com>
2025-07-01 01:50:25 +0700

    qteVisuals

---
+++ Assets/script/qteVisual.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class qteVisual : MonoBehaviour
+{
+    [SerializeField] private Sprite _spriteOpen;
+    [SerializeField] private Sprite _spriteClose;
+    Image image;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameController.instance.checkQTE())
+        {
+            image.sprite = _spriteOpen;
+        }
+        else
+        {
+            image.sprite = _spriteClose;
+        }
+    }
+}
